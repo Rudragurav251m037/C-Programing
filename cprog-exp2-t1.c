6a28d6b0e2348dc30a52798fcf7c4b4a85859618
Rudragurav251m037 <rudragurav@eng.rizvi.edu.in>
2025-11-24 13:28:47 +0530

    Add files via upload

---
+++ cprog-exp2-t1.c
@@ -0,0 +1,15 @@
+//Name- RudraGurav
+//uin-251M037
+// Div-F (f2)
+//Mechanical
+#include<stdio.h>
+int main()
+{ int number;
+    printf("enter number");
+    scanf("%d",&number);
+    if(number%2==0){
+        printf(" Number is even");
+    } else{printf("Number is odd");
+    }
+    return 0;
+}
