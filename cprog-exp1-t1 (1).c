2e199bba48b80fba8fb60b80c23e60700ad066fc
Rudragurav251m037 <rudragurav@eng.rizvi.edu.in>
2025-11-24 13:28:03 +0530

    Add files via upload

---
+++ cprog-exp1-t1 (1).c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+  int main() 
+ {
+     printf("name-Rudra_Ramdas_Gurav\n");
+ printf("age-17\n");
+ printf("class-FE\n");
+ printf("div-F\n");
+ printf("UIN-251M037\n");
+return 0;
+ }
