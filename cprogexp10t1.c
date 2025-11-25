5ea5cd02de32da06c3f959a6b4dc8478cdf7761f
Rudragurav251m037 <rudragurav@eng.rizvi.edu.in>
2025-11-25 09:35:35 +0530

    Add files via upload

---
+++ cprogexp10t1.c
@@ -0,0 +1,21 @@
+//Rudra Gurav 
+// FE Mechanical 
+//251M037
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+    int *p1, *p2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    p1 = &a;
+    p2 = &b;
+
+    sum = *p1 + *p2;
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
